1054e70b5e2e78ac7023907cd1d0f21c3c29b029
DLong <duclongbox@gmail.com>
2025-10-03 14:14:40 -0400

    set command sadd, srem, smember,sismember"

--- internal/core/commands_set.go
+++ internal/core/commands_set.go
@@ -1,16 +1,63 @@
-// package core
+package core
 
-// import (
-// 	"redisServer/internal/data_structure"
-// 	"errors"
+import (
+	"redisServer/internal/data_structure"
+	"errors"
 
-// )
+)
 
-// func cmdSADD(args []string) []byte{
+func cmdSADD(args []string) []byte{
+	if len(args) < 2{
+		return Encode(errors.New("(errors) ERR wrong number of arguments for 'SADD' command"),false)
 
-// }
+	}
+	key := args[0]
+	set, exist := setStore[key]
+	if !exist{
+		set = data_structure.NewSimpleSet(key)
+		setStore[key] = set
+	}
+	count := set.Add(args[1:]...)
+	return Encode(count,false)
 
-// func cmdSREM(args []string) []byte{
+}
 
-// }
+func cmdSREM(args []string) []byte{
+	if len(args) < 2{
+		return Encode(errors.New("(errors) ERR wrong number of arguments for 'SREM' command"),false)
 
+	}
+	key := args[0]
+	set,exist := setStore[key]
+	if !exist{
+		set = data_structure.NewSimpleSet(key)
+		setStore[key] = set
+	}
+	count := set.Rem(args[1:]...)
+	return Encode(count,false)
+}
+
+func cmdSMEMBERS(args []string) []byte{
+	if len(args) != 1{
+		return Encode(errors.New("(errors) ERR wrong number of arguments for 'SMEMBERS' command"),false)
+
+	}
+	key := args[0]
+	set,exist := setStore[key]
+	if !exist{
+		return Encode(make([]string,0),false)
+
+	}
+	return Encode(set.Members(),false)
+}
+func cmdSISMEMBER(args []string) []byte{
+	if len(args) != 2{
+		return Encode(errors.New("(errors) ERR wrong number of arguments for 'SISMEMBER' command"),false)
+	}
+	key := args[0]
+	set,exist := setStore[key]
+	if !exist{
+		return Encode(0,false)
+	}
+	return Encode(set.IsMember(args[1]),false)
+}
